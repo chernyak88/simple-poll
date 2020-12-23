4f642f4782448a12bfc7fd857e0af0f75e5995e0
Igor Chernyak <i_chernyak@inbox.ru>
2020-12-23 12:11:39 +0300

    make it better

--- polls.php
+++ polls.php
@@ -11,20 +11,16 @@ $value->question = 'Кто вы?';
 $value->answers = array('Интроверт','Экстраверт','Не знаю');
 $votes[$key] = $value;
 
-// $output = file_get_contents(dirname(__FILE__).'/'.$nameFile);
-// $output = json_decode($output, true);
-$votes[$current]->result = $output[$current];
-
-// if (isset($_COOKIE['polls'])) {
-//   $arrayPolls = explode(',',$_COOKIE['polls']);
-//   if (in_array($current, $arrayPolls)) {
-//     $output = file_get_contents(dirname(__FILE__).'/'.$nameFile);
-//     $output = json_decode($output, true);
-//     if (array_key_exists($current, $output)) {
-//       $votes[$current]->result = $output[$current];
-//     }
-//   }
-// }
+if (isset($_COOKIE['polls'])) {
+  $arrayPolls = explode(',',$_COOKIE['polls']);
+  if (in_array($current, $arrayPolls)) {
+    $output = file_get_contents(dirname(__FILE__).'/'.$nameFile);
+    $output = json_decode($output, true);
+    // if (array_key_exists($current, $output)) {
+    //   $votes[$current]->result = $output[$current];
+    // }
+  }
+}
 
 echo json_encode($votes[$current]);
 
